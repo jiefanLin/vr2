988dbe3a4eb06b2c340de60e5320a341aad4e6bc
jiefan <1721985237@qq.com>
2017-11-22 17:07:51 +0800

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Administrator on 2017/10/30.
+ */
+$(function(){
+    $("#major-mk3").slide({
+        autoPage:false,
+        titCell:'.d2 li',
+        mainCell:'.d3 ul'
+        
+
+
+    })
+});
